f01939af4ff00b3c0a39f3bb3e4546b13d241999
0raffay <0,abdulraffay@gmail.com>
2023-11-26 14:59:28 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import '../src/assets/css/style.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <div className='main'>
+    </div>
+    </>
+  )
+}
+
+export default App
